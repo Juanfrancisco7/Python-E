05642231c99d1d9facb5010d07a52d846a1803df
Juanfrancisco7 <juanbencomo25@gmail.com>
2025-07-10 16:43:01 +0200

    Class06F

--- Hello-Python-main/Basic/05_tuples.py
+++ Hello-Python-main/Basic/05_tuples.py
@@ -18,9 +18,9 @@ print(my_tuple[-1])
 # print(my_tuple[4]) IndexError
 # print(my_tuple[-6]) IndexError
 
-print(my_tuple.count("Brais"))
-print(my_tuple.index("Moure"))
-print(my_tuple.index("Brais"))
+print(my_tuple.count("Juan"))
+print(my_tuple.index("Francisco"))
+print(my_tuple.index("Juan"))
 
 # my_tuple[1] = 1.80 'tuple' object does not support item assignment
 
@@ -38,7 +38,7 @@ print(my_sum_tuple[3:6])
 my_tuple = list(my_tuple)
 print(type(my_tuple))
 
-my_tuple[4] = "MoureDev"
+my_tuple[4] = "JuanFrancisco7"
 my_tuple.insert(1, "Azul")
 my_tuple = tuple(my_tuple)
 print(my_tuple)
